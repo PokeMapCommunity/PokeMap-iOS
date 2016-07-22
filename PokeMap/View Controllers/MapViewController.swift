a8b50e8d9957fe1da8c0e15f8b8b0f07763c01b1
Keaton Burleson <keaton.burleson@me.com>
2016-07-22 14:05:34 -0400

    Made some general UI changes: - Darkened the UI - Added the 'Pokemon Go' icon - Modified the title to reflect Pokemon styling - Moved the center button to the center to make way for a settings icon - Separated the identifier from the name in the cell - Added a title to the watch list - Made the 'Scan' icon a magnifying glass versus text - Added 'Center' icon - Added 'Watchlist' icon
    Minor UI Changes (on top of my current ones):
    - Added two more icons (Watchlist and Center)
    - Recolored the title bar
    - Repositioned the labels in the tableview cells

--- PokeMap/View Controllers/MapViewController.swift
+++ PokeMap/View Controllers/MapViewController.swift
@@ -14,89 +14,6 @@ import Permission
 import MKMapView_ZoomLevel
 
 class MapViewController: UIViewController {
-<<<<<<< ours
-
-  @IBOutlet private weak var mapView: MKMapView!
-
-  private let viewModel = PokemonMapViewModel()
-  private var centerLocation: Variable<CLLocationCoordinate2D>!
-  private var userLocation: Variable<CLLocationCoordinate2D>!
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    centerLocation = Variable(mapView.centerCoordinate)
-    userLocation = Variable(mapView.userLocation.coordinate)
-    bindViewModel()
-    Permission.LocationAlways.request { _ in
-    }
-  }
-
-  private func bindViewModel() {
-    viewModel.viewModels
-      .subscribeNext { [weak self] viewModels in
-        guard let `self` = self else { return }
-        self.setupAnnotations(viewModels)
-      }.addDisposableTo(rx_disposeBag)
-
-    centerLocation.asObservable()
-      .throttle(1, scheduler: MainScheduler.instance)
-      .subscribeNext { [weak self] location in
-        guard let `self` = self else { return }
-        self.loadPokemons(location)
-      }.addDisposableTo(rx_disposeBag)
-
-    userLocation.asObservable()
-      .throttle(1, scheduler: MainScheduler.instance)
-      .subscribeNext { [weak self] location in
-        guard let `self` = self else { return }
-        self.loadPokemons(location)
-      }.addDisposableTo(rx_disposeBag)
-    
-    userLocation.asObservable()
-      .throttle(1, scheduler: MainScheduler.instance).take(1)
-      .subscribeNext { [weak self] location in
-        self?.mapView.setCenterCoordinate(location, zoomLevel: 14, animated: true)
-    }.addDisposableTo(rx_disposeBag)
-  }
-
-  func loadPokemons(location: CLLocationCoordinate2D) {
-    viewModel.loadPokemons(location.latitude, longitude: location.longitude, jobId: nil)
-      .subscribe()
-      .addDisposableTo(rx_disposeBag)
-  }
-
-  @IBAction func openPokemonGo() {
-    UIApplication.sharedApplication()
-      .openURL(NSURL(string: "b335b2fc-69dc-472c-9e88-e6c97f84091c-3://")!)
-  }
-
-  @IBAction func center() {
-    mapView.setCenterCoordinate(mapView.userLocation.coordinate, animated: true)
-  }
-
-  @IBAction func scan() {
-    let coordinate = mapView.centerCoordinate
-    Network
-      .request(API.Scan(latitude: coordinate.latitude, longitude: coordinate.longitude))
-      .mapObject(ScanJob)
-      .flatMap {
-        return self.viewModel
-          .loadPokemons(coordinate.latitude, longitude: coordinate.longitude, jobId: $0.jobId)
-      }.take(1)
-      .subscribe()
-      .addDisposableTo(rx_disposeBag)
-  }
-
-  private func setupAnnotations(viewModels: [PokemonMapItemViewModel]) {
-    let annotations = mapView.annotations.flatMap { $0 as? PokemonAnnotation }
-    let expiredAnnotations = annotations.filter { $0.expired }
-    mapView.removeAnnotations(expiredAnnotations)
-    let validAnnotations = mapView.annotations.flatMap { $0 as? PokemonAnnotation }
-    let pokemonIds = validAnnotations.map { $0.identifier }
-    let newViewModels = viewModels.filter { !pokemonIds.contains($0.identifier) }
-    mapView.addAnnotations(newViewModels.map { $0.annotation })
-  }
-=======
 	
 	@IBOutlet private weak var mapView: MKMapView!
 	
@@ -165,7 +82,6 @@ class MapViewController: UIViewController {
 	}
 	
 	@IBAction func center() {
-		mapView.userTrackingMode = .FollowWithHeading
 		mapView.setCenterCoordinate(mapView.userLocation.coordinate, animated: true)
 	}
 	
@@ -191,23 +107,22 @@ class MapViewController: UIViewController {
 		let newViewModels = viewModels.filter { !pokemonIds.contains($0.identifier) }
 		mapView.addAnnotations(newViewModels.map { $0.annotation })
 	}
->>>>>>> theirs
 }
 
 extension MapViewController: MKMapViewDelegate {
-  func mapView(mapView: MKMapView, didUpdateUserLocation userLocation: MKUserLocation) {
-    self.userLocation.value = userLocation.coordinate
-  }
-
-  func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation)
-    -> MKAnnotationView? {
-      guard let pokemonAnnotation = annotation as? PokemonAnnotation else {
-        return nil
-      }
-      return pokemonAnnotation.annotationView
-  }
-
-  func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
-    centerLocation.value = mapView.centerCoordinate
-  }
+	func mapView(mapView: MKMapView, didUpdateUserLocation userLocation: MKUserLocation) {
+		self.userLocation.value = userLocation.coordinate
+	}
+	
+	func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation)
+		-> MKAnnotationView? {
+			guard let pokemonAnnotation = annotation as? PokemonAnnotation else {
+				return nil
+			}
+			return pokemonAnnotation.annotationView
+	}
+	
+	func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+		centerLocation.value = mapView.centerCoordinate
+	}
 }
